4b05bed2677e101676c0d6d2e2124417d934931e
Smelch228 <salamandr258@gmail.com>
2023-11-15 00:05:33 +0200

    auth resource init

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { HashService } from './hash.service';
+
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService, HashService],
+})
+export class AuthModule {}

02221b9d4f92b602b3afc92fcba5eba32b3f980b
Andrei Griu <salamandr258@gmail.com>
2023-12-05 01:45:53 +0200

    implemented some crud functions

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,26 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { VotingService } from '../voting/voting.service';
 
+//TODO: Создай пагинацию и фильтрацию, плюс внедри счётчик голосов.
 @Injectable()
 export class PostService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly votingService: VotingService,
+  ) {}
+
+  async create(createPostDto: CreatePostDto) {
+    try {
+      return await this.prisma.post.create({
+        data: createPostDto,
+      });
+    } catch (error) {
+      throw new Error('Error in creating post: ' + error.message);
+    }
   }
 
-  findAll() {
-    return `This action returns all post`;
+  async findAll() {
+    await this.prisma.post.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
+  async findOne(id: number) {
+    const post = await this.prisma.post.findUnique({
+      where: { id },
+    });
+    if (!post) {
+      throw new NotFoundException(`Post with ID ${id} not found`);
+    }
+    return post;
   }
 
-  update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
+  async update(id: number, updatePostDto: UpdatePostDto) {
+    try {
+      return await this.prisma.post.update({
+        where: { id },
+        data: updatePostDto,
+      });
+    } catch (error) {
+      throw new NotFoundException(`Post with ID ${id} not found`);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} post`;
+  async remove(id: number) {
+    try {
+      await this.prisma.post.delete({
+        where: { id },
+      });
+      return `Post with ID ${id} was deleted`;
+    } catch (error) {
+      throw new NotFoundException(
+        `Post with ID ${id} not found or could not be deleted`,
+      );
+    }
   }
 }

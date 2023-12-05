dca99e55ac6e5e5bf516d07ecb637c9c5a3f5d15
Smelch228 <salamandr258@gmail.com>
2023-12-05 11:54:27 +0200

    implemented custom guard for roles

---
+++ src/common/guards/roles.guard.ts
@@ -0,0 +1,22 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private readonly allowedRoles: string[]) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    if (!user || !this.allowedRoles.includes(user.role)) {
+      throw new ForbiddenException('Access Denied');
+    }
+
+    return true;
+  }
+}

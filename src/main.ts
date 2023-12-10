2596004c6e278339decffbb2d339ce210074ff98
Smelch228 <salamandr258@gmail.com>
2023-12-10 19:57:42 +0200

    changed config

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix('v1');
   await app.listen(3000);
 }
